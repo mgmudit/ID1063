ccf64fefe829b3593e436d4d59fc77630b7e1886
Mudit Gupta <YOUR_GITHUB_EMAIL>
2026-09-09 16:52:55 +0530

    Lab 6

--- labs/qw6.c
+++ labs/qw6.c
@@ -1,28 +1,42 @@
 //Code by Mudit
 //Date: 09/09/2026
 #include <stdio.h>
+#include <stdlib.h>
+
+// Dynamically allocate memory for n characters and '\0'
+char *createChar(int n)
+{
+    char *a;
+
+    a = (char *)malloc((n + 1) * sizeof(char));
+
+    return a;
+}
 
 int main()
 {
     int n;
 
-    printf("Enter integer n: ");
+    printf("Enter n: ");
     scanf("%d", &n);
 
-    char str[n + 1];
+    // Create a dynamically allocated character array
+    char *str = createChar(n);
 
     printf("Enter %d characters:\n", n);
 
-    // Read exactly n characters
+    // Store n characters in the array
     for (int i = 0; i < n; i++)
     {
         scanf(" %c", &str[i]);
     }
 
-    // Add null character to make it a valid C string
+    // Null character makes the character array a valid C string
     str[n] = '\0';
 
     printf("String: %s\n", str);
+
+    free(str);
 
     return 0;
 }
