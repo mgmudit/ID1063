999a844013ce4028b56a1338afde8e4e4aa03da1
Mudit Gupta <YOUR_GITHUB_EMAIL>
2026-09-09 16:34:24 +0530

    Lab 6

--- labs/qw5.c
+++ labs/qw5.c
@@ -3,19 +3,21 @@
 #include <stdio.h>
 #include <string.h>
 
+// Function to check whether a string is a palindrome
 int isPalindrome(char str[])
 {
     int len = strlen(str);
 
+    // Compare characters from the beginning and the end
     for (int i = 0; i < len / 2; i++)
     {
         if (str[i] != str[len - 1 - i])
         {
-            return 0;
+            return 0;   // Not a palindrome
         }
     }
 
-    return 1;
+    return 1;   // Palindrome
 }
 
 int main()
@@ -23,10 +25,14 @@ int main()
     char str[100];
 
     printf("Input: ");
+
+    // fgets is used to accept strings containing spaces
     fgets(str, sizeof(str), stdin);
 
+    // Remove the newline character added by fgets
     str[strcspn(str, "\n")] = '\0';
 
+    // Call the palindrome checking function
     if (isPalindrome(str))
         printf("Output: Palindrome\n");
     else
